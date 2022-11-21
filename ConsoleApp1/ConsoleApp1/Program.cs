72277904822e9dbbdadd1464199bff1d1bd93cd5
Evgenia18 <118425098+Evgenia18@users.noreply.github.com>
2022-11-21 18:14:15 +0900

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using factionfile;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            flot fuct=new flot();
+            fuct.HelloWord("Евгения");
+            Console.WriteLine($"Через 10 лет вам будет - {fuct.AgeToTen(30)}");
+            Console.ReadLine();
+        }
+
+
+    }
+
+}
---
+++ factionfile/factionfile/flot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factionfile
+{
+    public class flot
+    {
+        public void HelloWord(string name)
+        {
+
+            Console.WriteLine($"Привет,{name}"); 
+        }
+
+        public int AgeToTen (int age)
+        {
+            return age + 10;
+        }
+
+        public dynamic AnsParse(byte[] Array)
+        {
+            switch (Array[0])
+            {
+                case 1:
+                    return 1; // возвращаем число
+                case 2:
+                    return false; // возвращаем булевое значение
+            }
+            return "no result"; // возвращаем строку
+        }
+
+
+        public string Reverse(string s)
+        {
+            string s2 = "";
+            int len = s.Length;
+
+            for (int i = 0; i < len; i++)
+                s2 += s[len - i - 1];
+
+            return s2;
+        }
+    }
+}
